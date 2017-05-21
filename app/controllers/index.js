5452bb276ae756608600e0d1dd2b1a3144297972
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-05-21 19:47:20 +0300

    Added index controller

---
+++ app/controllers/index.js
@@ -0,0 +1,11 @@
+import Controller from 'ember-controller';
+export default Controller.extend({
+
+  /**
+   * Contains a collection of items to render
+   * @type {Array}
+   * @public
+   */
+  items: []
+
+});
