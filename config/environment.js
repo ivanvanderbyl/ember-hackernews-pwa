b668d2f26806ade422afec7e6f1d7824db3fadc1
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-05-21 19:47:57 +0300

    Cleaned up environment, disabled prototype extensions

--- config/environment.js
+++ config/environment.js
@@ -12,22 +12,14 @@ module.exports = function(environment) {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       },
-      EXTEND_PROTOTYPES: {
-        Object: false,
-        Array: false,
-        Number: false,
-        Date: false
-      }
+      EXTEND_PROTOTYPES: false,
+      ENABLE_DS_FILTER: true
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
     }
-  };
-
-  ENV.firebase = {
-    databaseURL: 'https://hacker-news.firebaseio.com'
   };
 
   if (environment === 'development') {
