b3b7e1c9b9343bb02af3d87f63297331288ce619
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-05-21 19:47:43 +0300

    Added manifest

---
+++ config/manifest.js
@@ -0,0 +1,39 @@
+/* eslint-env node*/
+/* eslint-disable camelcase */
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/san650/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: 'Ember HackerNews',
+    short_name: 'HN',
+    description: 'Ember based HackerNews Client as a progressive web app',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#dd6a58',
+    icons: [
+      {
+        src: '/assets/images/ember-hn192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/assets/images/ember-hn512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: '/assets/images/ember-hn180.png',
+        sizes: '180x180',
+        type: 'image/png',
+        targets: ['apple']
+      }
+    ],
+
+    apple: {
+      statusBarStyle: 'black-translucent'
+    }
+  };
+};
