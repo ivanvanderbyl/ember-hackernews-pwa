16e9a7376fb6f5f01d3c79f44ef73c654e19f8e0
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-05-23 13:41:54 +0300

    Show loading when fetching app

---
+++ app/routes/application.js
@@ -0,0 +1,11 @@
+import Route from 'ember-route';
+export default Route.extend({
+
+  activate() {
+    if (typeof FastBoot === 'undefined') {
+      let preloaderEl = document.getElementById('preloader');
+      preloaderEl.parentNode.removeChild(preloaderEl);
+    }
+  }
+
+});
