f264668250ec17d6bf779e65fd60b5ef99a9e10c
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-08-26 20:36:35 +1000

    fastboot server

---
+++ server/main.js
@@ -0,0 +1,6 @@
+const FastBootAppServer = require('fastboot-app-server');
+let server = new FastBootAppServer({
+  distPath: 'dist',
+  gzip: true // Optional - Enables gzip compression.
+});
+server.start();
