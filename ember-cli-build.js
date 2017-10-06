82b779ac97c79d49126b0ee9c095d2431e8d4358
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-10-06 17:36:45 +0200

    Format

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,64 +1,79 @@
 /* eslint-env node */
-'use strict';
+'use strict'
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const autoprefixer = require('autoprefixer');
-const targets = require('./config/targets');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app')
+const autoprefixer = require('autoprefixer')
+const targets = require('./config/targets')
 
 module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {
-    'ember-cli-babel': {
-      includePolyfill: false
-    },
+	let app = new EmberApp(defaults, {
+		'ember-cli-babel': {
+			includePolyfill: false,
+		},
 
-    'babel': {
-      plugins: ['minify-dead-code-elimination']
-    },
+		babel: {
+			plugins: ['minify-dead-code-elimination'],
+		},
 
-    // 'asset-cache': {
-    //   version: '5'
-    // },
+		'asset-cache': {
+			version: '5.1',
+		},
 
-    fingerprint: {
-      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'svg']
-      // prepend: 'https://hackernews.io/'
-    },
+		fingerprint: {
+			extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'svg'],
+			// prepend: 'https://hackernews.io/'
+		},
 
-    postcssOptions: {
-      compile: {
-        enabled: false
-      },
-      filter: {
-        enabled: true,
-        plugins: [
-          {
-            module: autoprefixer,
-            options: {
-              browsers: targets.browsers
-            }
-          }
-        ]
-      }
-    },
+		postcssOptions: {
+			compile: {
+				enabled: false,
+			},
+			filter: {
+				enabled: true,
+				plugins: [
+					{
+						module: autoprefixer,
+						options: {
+							browsers: targets.browsers,
+						},
+					},
+				],
+			},
+		},
 
-    vendorFiles: {
-      'jquery.js': null,
-      'vendor.css': null
-    }
-  });
+		minifyHTML: {
+			enabled: true,
+			htmlFiles: ['index.html'],
+			minifierOptions: {
+				collapseWhitespace: true,
+				removeComments: true,
+				minifyJS: true,
+				minifyCSS: true,
+			},
+		},
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+		inlineContent: {
+			'loader-styles': 'app/styles/loader.css',
+		},
 
-  return app.toTree();
-};
+		vendorFiles: {
+			'jquery.js': null,
+			'vendor.css': null,
+		},
+	})
+
+	// Use `app.import` to add additional libraries to the generated
+	// output files.
+	//
+	// If you need to use different assets in different
+	// environments, specify an object as the first parameter. That
+	// object's keys should be the environment name and the values
+	// should be the asset to use in that environment.
+	//
+	// If the library that you are including contains AMD or ES6
+	// modules that you would like to import into your application
+	// please specify an object with the list of modules as keys
+	// along with the exports of each module as its value.
+
+	return app.toTree()
+}
